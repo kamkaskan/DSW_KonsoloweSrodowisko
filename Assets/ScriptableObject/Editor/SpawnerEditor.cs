4fd81e9a77fb640223d8e8298b2b48ccf14cf03d
kamkaskan <kamkaskan@gmail.com>
2023-01-08 09:30:18 +0100

    Add files via upload

---
+++ Assets/ScriptableObject/Editor/SpawnerEditor.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.EditorTools;
+namespace JK.ObjectSpawner.Editor
+{
+    [EditorTool("Object Spawner")]
+    public class ObjectSpawnerEditorTool : EditorTool
+    {
+        [NonSerialized] private Rect toolWindowRect = new Rect(10, 0,
+        260f, 0f);
+        private Spawner selectedSpawner;
+        [NonSerialized] private Spawner selectedSpawnerMemory; //If changed, than update spawn count
+        private float spawnRadius = 2;
+        private bool onlyEdge = false; //New Spawn method, spawn only on circle edge
+        private bool overrideSpawnCount = false; //An option to change spawn count
+        private int overridenSpawnCount = 0;
+        [NonSerialized] private bool mouseDown;
+        [NonSerialized] private Vector2 mouseDownPosition;
+        [NonSerialized] private GUIContent toolIcon;
+        public override GUIContent toolbarIcon
+        {
+            get
+            {
+                if (toolIcon == null) toolIcon = EditorGUIUtility.IconContent("GameObject On Icon", "Object Spawner");
+                return toolIcon;
+            }
+        }
+        public void DrawToolWindow(int id)
+        {
+            selectedSpawner = EditorGUILayout.
+            ObjectField(selectedSpawner, typeof(Spawner), false) as Spawner;
+            spawnRadius = EditorGUILayout.FloatField("Radius", spawnRadius);
+            onlyEdge = EditorGUILayout.Toggle("Only Edge", onlyEdge);
+            overrideSpawnCount = EditorGUILayout.Toggle("Override Spawn Count", overrideSpawnCount);
+            if (overrideSpawnCount) 
+            {
+                overridenSpawnCount = EditorGUILayout.IntField("Spawn Count", overridenSpawnCount);
+            }
+        }
+        public override void OnToolGUI(EditorWindow window)
+        {
+            if (selectedSpawnerMemory != selectedSpawner) 
+            {
+                selectedSpawnerMemory = selectedSpawner;
+                if (selectedSpawner != null) overridenSpawnCount = selectedSpawner.spawnCount;
+            }
+            toolWindowRect.y = window.position.height - toolWindowRect.
+            height - 10;
+            toolWindowRect = GUILayout.Window(45, toolWindowRect,
+            DrawToolWindow, "Object Spawner");
+            // Event logic
+            var ray = HandleUtility.GUIPointToWorldRay(mouseDown ?
+            mouseDownPosition : Event.current.mousePosition);
+            bool hitGround = Physics.Raycast(ray, out var result, 100);
+            if (hitGround)
+            {
+                Handles.DrawWireDisc(result.point, Vector3.up, spawnRadius);
+            }
+
+            var controlId = EditorGUIUtility.GetControlID(FocusType.Passive);
+            switch (Event.current.type)
+            {
+                case EventType.MouseDown:
+                if (Event.current.button == 0 && Event.current.modifiers == EventModifiers.None)
+                {
+                    GUIUtility.hotControl = controlId;
+                    mouseDown = true;
+                    mouseDownPosition = Event.current.mousePosition;
+                    Event.current.Use();
+                }
+                break;
+                case EventType.MouseDrag:
+                if (mouseDown)
+                {
+                    spawnRadius += EditorGUIUtility.PixelsToPoints(Event.current.delta).x / 100;
+                    window.Repaint();
+                }
+                break;
+                case EventType.MouseMove:
+                window.Repaint();
+                break;
+                case EventType.MouseLeaveWindow:
+                case EventType.MouseUp:
+                if (mouseDown && hitGround && selectedSpawner)
+                {
+                    selectedSpawner.SpawnObjects(result.point, spawnRadius, onlyEdge, overrideSpawnCount ? overridenSpawnCount : 0);
+                }
+                mouseDown = false;
+                break;
+            }
+        }
+    }
+}
---
+++ Assets/ScriptableObject/Runtime/Spawner.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+namespace JK.ObjectSpawner
+{
+    [CreateAssetMenu(fileName = "Spawner", menuName = "ObjectSpawner/Spawner")]
+    public class Spawner : ScriptableObject {
+        public int spawnCount = 10;
+        public GameObject[] prefabs;
+        [Header("Auto Ground")]
+        public bool autoGround = true;
+        public float raycastHeight = 10;
+        public float groundOffset;
+        public void SpawnObjects(Vector3 position, float radius, bool onlyEdge = false, int overrideSpawnCount = 0) {
+            var stampParentObject = new GameObject("Stamp - " + name);
+            stampParentObject.transform.position = position;
+            #if UNITY_EDITOR
+            UnityEditor.Undo.RegisterCreatedObjectUndo(stampParentObject, "Create " + stampParentObject.name);
+            #endif
+            int prefabSpawnCount = (overrideSpawnCount == 0) ? spawnCount : overrideSpawnCount;
+            for (int i = 0; i < prefabSpawnCount; i++)
+            {
+                var randomPrefab = prefabs[Random.Range(0, prefabs.Length)];
+                var randomPosition2D = onlyEdge ? Random.insideUnitCircle.normalized : Random.insideUnitCircle;
+                randomPosition2D *= radius;
+                var targetPositionX = position.x + randomPosition2D.x;
+                var targetPositionZ = position.z + randomPosition2D.y;
+                var targetPositionY = position.y;
+                var targetPosition = new Vector3(targetPositionX, targetPositionY, targetPositionZ);
+                if (autoGround)
+                {
+                var raycastOrigin = targetPosition;
+                raycastOrigin.y += raycastHeight;
+                if (Physics.Raycast(raycastOrigin, Vector3.down, out var result, raycastHeight))
+                targetPosition.y = result.point.y + groundOffset;
+                }
+                #if UNITY_EDITOR
+                var target = UnityEditor.PrefabUtility.
+                InstantiatePrefab(randomPrefab, stampParentObject.transform) as GameObject;
+                target.transform.position = targetPosition;
+                #else
+                Instantiate(randomPrefab, targetPosition, Quaternion.identity, stampParentObject.transform);
+                #endif
+            } 
+        }
+    }
+}
